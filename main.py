e5466882b7f9a3438fa947a178b0a8a63c93a2ee
Juan José Luarca <124697529+juanjluarca@users.noreply.github.com>
2023-02-14 09:42:29 -0600

    Modificación de main y .gitignore
    Se agregó código al archivo main y se agregó una carpeta a ignorar al archivo .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
 print('Mi nombre es Juan José Luarca Rodríguez, Carnet: 1512623')
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
